9c2425e980cc11c172a084bc62939918541e0693
hamza0786 <55581835+hamza0786@users.noreply.github.com>
2023-07-26 20:48:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const http = require('http');
 const app = express();
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+server.listen(port, () => {
+	console.log('started on port ${port}');
+});
 
 const connection = mysql.createConnection({
 	host : 'sql6.freemysqlhosting.net',
